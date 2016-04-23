29763a09d12b2a722c4c88271a2fc8eb44a5414a
Florian brandes <florian.brandes@gmx.de>
2016-04-23 12:29:51 +0200

    Initial

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    system("ls -l >test.txt");
+    cout << ifstream("test.txt").rdbuf();
+    return 0;
+}
